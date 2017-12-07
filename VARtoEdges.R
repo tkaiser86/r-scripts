03e471c8c9251818fb7bc81cdea739daa3fd90bc
tkaiser86 <kaiserti@stud.sbg.ac.at>
2017-12-07 14:20:03 +0100

    VARtoEdges can now handle models with more than 1 lag
    By now, "handle" means "average the coefficients from different lags".

--- VARtoEdges.R
+++ VARtoEdges.R
@@ -4,23 +4,23 @@
 # bonferroni: Bonferroni-corrects alpha levels for temporal and contemporal associations
 # granger: include only Granger-causal temporal associations
 require(vars)
-VARtoEdges <- function(x, nonsig = F, alpha = .05, bonferroni = T, granger = F){
+VARtoEdges <- function(x, nonsig = F, alpha = .05, bonferroni = T){
   VARsum <- summary(x)
   
   if(bonferroni){
-    alphacor <- alpha / ((x$K*(x$K-1))/2) # Correct for every pair
-    alphareg <- alpha / x$K # Correct for every predictor
+    alphacor <- alpha / (((x$K*x$p)*((x$K*x$p)-1))/2) # Correct for every pair
+    alphareg <- alpha / x$p*x$K # Correct for every predictor
   } else {
     alphacor = alpha
     alphareg = alpha
   }
   
   edgelist <- list()
-  temporal.edges <- matrix(nrow = x$K, ncol = x$K)
+  temporal.edges <- matrix(nrow = x$K*x$p, ncol = x$K)
   
-  if(!nonsig){significance <- matrix(nrow = x$K, ncol = x$K)}
+  if(!nonsig){significance <- matrix(nrow = x$K*x$p, ncol = x$K)}
   for(i in 1:x$K){
-    for(j in 1:x$K){
+    for(j in 1:(x$K * x$p)){
       temporal.edges[j,i] <- VARsum$varresult[[i]]$coefficients[j]
       if(!nonsig){
         significance[j,i] <- VARsum$varresult[[i]]$coefficients[j,4]
@@ -30,9 +30,15 @@ VARtoEdges <- function(x, nonsig = F, alpha = .05, bonferroni = T, granger = F){
   if(!nonsig){
     significance <- significance < alphareg
     temporal.edges <- temporal.edges * significance}
-  df.edges <- as.data.frame(temporal.edges)
-  rownames(df.edges) <- colnames(x$datamat)[1:x$K]
-  colnames(df.edges) <- colnames(x$datamat)[1:x$K]
+  resmatrix <- matrix(nrow = x$K, ncol = x$K)
+  for(i in 1:x$K){
+    for(j in 1:x$K){
+  resmatrix[j,i] <- mean(temporal.edges[seq((1+j), (x$K*x$p), x$K),i])
+    }}
+  df.edges <- as.data.frame(
+    resmatrix)
+  rownames(df.edges) <- colnames(x$datamat)[1:(x$K)]
+  colnames(df.edges) <- colnames(x$datamat)[1:(x$K)]
   edgelist$temporal <- df.edges
   
   contemp.edges <- VARsum$corres
@@ -48,15 +54,6 @@ VARtoEdges <- function(x, nonsig = F, alpha = .05, bonferroni = T, granger = F){
     contemp.edges <- contemp.edges * significance
   }
   
-  if(granger){
-    granger <- NULL
-    varnames <- colnames(VARsum$corres)
-    for(i in 1:x$K){
-      granger[i] <- vars::causality(x, cause = varnames[i])$Granger$p.value
-    }
-    granger.causality <- matrix(rep(granger, x$K), ncol = 5) < .05
-    contemp.edges <- contemp.edges * granger.causality
-  }
   edgelist$contemp <- contemp.edges
   return(edgelist)
 }
