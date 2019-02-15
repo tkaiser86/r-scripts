b75dd9383e878225e8cfe02e872c4b7abeceb649
tkasci <Tim.Kaiser@sbg.ac.at>
2019-02-15 09:38:15 +0100

    Update VARtoEdges.R

--- VARtoEdges.R
+++ VARtoEdges.R
@@ -1,59 +1,33 @@
 # Convert a lag 1 VAR model from vars to edge lists processable by qgraph. Bonferroni-corrects all coefficients.
-# nonsig: boolean. Include non-significant parameters in edge lists.
-# alpha: alpha level 
-# bonferroni: Bonferroni-corrects alpha levels for temporal and contemporal associations
+# p.adjust = Adjust p-values for VAR models using the methods supported by stats::p.adust
+# see ?p.adjust for more information
 # granger: include only Granger-causal temporal associations
 require(vars)
-VARtoEdges <- function(x, nonsig = F, alpha = .05, bonferroni = T){
-  VARsum <- summary(x)
+VARtoEdges <- function(x, p.adjust = "none", granger = F) {
+  var.sum <- summary(x)
+  nvar <- x$K
+  maxp <- x$p
+  mat.list <- list()
   
-  if(bonferroni){
-    alphacor <- alpha / (((x$K*x$p)*((x$K*x$p)-1))/2) # Correct for every pair
-    alphareg <- alpha / x$p*x$K # Correct for every predictor
-  } else {
-    alphacor = alpha
-    alphareg = alpha
+  for (a in 1:maxp) {
+    mat.list[[a]] <- matrix(ncol = nvar, nrow = nvar)
+    colnames(mat.list[[a]]) <- var.sum$names
+    rownames(mat.list[[a]]) <- var.sum$names
+    for (b in 1:nvar) {
+      coefs <- data.frame(var.sum$varresult[[b]]$coefficients)
+      current <- coefs[grep(a, row.names(coefs)), c(1, 4)]
+      significant <- p.adjust(current[, 2], method = p.adjust) < .05
+      mat.list[[a]][, b] <- current[, 1] * significant
+    }
   }
   
-  edgelist <- list()
-  temporal.edges <- matrix(nrow = x$K*x$p, ncol = x$K)
-  
-  if(!nonsig){significance <- matrix(nrow = x$K*x$p, ncol = x$K)}
-  for(i in 1:x$K){
-    for(j in 1:(x$K * x$p)){
-      temporal.edges[j,i] <- VARsum$varresult[[i]]$coefficients[j]
-      if(!nonsig){
-        significance[j,i] <- VARsum$varresult[[i]]$coefficients[j,4]
-      }
-    }}
-  
-  if(!nonsig){
-    significance <- significance < alphareg
-    temporal.edges <- temporal.edges * significance}
-  resmatrix <- matrix(nrow = x$K, ncol = x$K)
-  for(i in 1:x$K){
-    for(j in 1:x$K){
-  resmatrix[j,i] <- mean(temporal.edges[seq(j, (x$K*x$p), x$K), i])
-    }}
-  df.edges <- as.data.frame(
-    resmatrix)
-  rownames(df.edges) <- colnames(x$datamat)[1:(x$K)]
-  colnames(df.edges) <- colnames(x$datamat)[1:(x$K)]
-  edgelist$temporal <- df.edges
-  
-  contemp.edges <- VARsum$corres
-  if(!nonsig){
-    significance <- matrix(nrow= x$K, ncol = x$K)
-    for(i in 1:x$K){
-      for(j in 1:x$K){
-        correlation <- VARsum$corres[j,i]
-        tVal <- correlation / sqrt((1-correlation^2) / (x$obs-2))
-        pVal <- 1 - pt(tVal, x$obs-2)
-        significance[j,i] <- pVal < alphacor
-      }}
-    contemp.edges <- contemp.edges * significance
+  if(granger){
+    granger.causality <- vector()
+    for(i in 1:length(summary(varmodel)$names)){
+      granger.causality[i] <- causality(varmodel, cause = summary(varmodel)$names[i])$Granger$p.value  
+    }
+    names(granger.causality) <- summary(varmodel)$names
   }
   
-  edgelist$contemp <- contemp.edges
-  return(edgelist)
+  return(mat.list)
 }
